5d6f0977e34d154cd71c75520dd8c70ff3520686
itsuc <138981985+itsuc@users.noreply.github.com>
2024-02-09 23:33:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+//dice1
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomImgSource1 = "images/dice" + randomNumber1 + ".png";
+
+document.querySelectorAll("img")[0].setAttribute("src", randomImgSource1);
+
+//dice2
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImgSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImgSource2);
+
+//outcome
+
+if(randomNumber1>randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+}
+
+else if(randomNumber1<randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
+
+else{
+    document.querySelector("h1").innerHTML = "Draw!";
+}
